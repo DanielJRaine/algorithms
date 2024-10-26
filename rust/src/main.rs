d58580ceeba1fbb659b9aefb7adc138775c3429b
Daniel Raine <daniel.raine@modernatx.com>
2024-10-26 10:43:40 -0400

    restructure into submodules

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,23 +1,33 @@
 #![feature(isqrt, iter_advance_by)]
+mod challenges {
+    pub mod two_crystal_balls;
+}
 
-// use crate::linear::linear;
-use crate::binary::binary;
-use crate::two_crystal_balls::two_crystal_balls;
+mod search {
+    pub mod binary;
+    pub mod linear;
+}
 
-mod binary;
-mod linear;
-mod two_crystal_balls;
+mod sort {
+    pub mod bubble;
+}
 
 fn main() {
     // let needle = 11;
     // let haystack = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     // let result = search(needle, haystack, binary::<usize>);
     // println!("{:?}", result)
-	
-	let b = two_crystal_balls([false,false,false,false,false,false,false,false,false,true]);
-	println!("{b}");
+
+	// let b = two_crystal_balls([false,false,false,false,false,false,false,false,false,true]);
+	// println!("{b}");
+
+
 }
 
 fn search(needle: usize, haystack: [usize; 10], strategy: fn(usize, [usize; 10]) -> bool) -> bool {
     strategy(needle, haystack)
+}
+
+fn sort(collection: [usize; 10], strategy: fn([usize; 10]) -> bool) -> bool {
+    strategy(collection)
 }
---
+++ rust/src/sort/bubble.rs
@@ -0,0 +1,3 @@
+pub fn bubble(collection: [usize; 10]) {
+    todo!()
+}
